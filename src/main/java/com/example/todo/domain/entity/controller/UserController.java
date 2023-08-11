5bcf562e23c2a5d2b1b3875e407edc45f2ce7d6f
YunByungil <dbsquddlfz@naver.com>
2023-08-11 15:36:32 +0900

    first commit

---
+++ src/main/java/com/example/todo/domain/entity/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.todo.domain.entity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/")
+    public String home() {
+        return "hi";
+    }
+}
