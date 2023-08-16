0f26f153a125e85ae77f9b6d4e0f4cd2a8cc8da0
BlackHan26 <128695414+BlackHan26@users.noreply.github.com>
2023-08-16 10:57:25 +0900

    Feat: ResponseDto 추가

---
+++ src/main/java/com/example/todo/api/ResponseDto.java
@@ -0,0 +1,12 @@
+package com.example.todo.api;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class ResponseDto {
+
+    private final String message;
+
+}
