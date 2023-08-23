a2ddcd3115e8f9bd86648d9439aa2db6f7ea84ff
RiverCastle <rkdtjd12@gmail.com>
2023-08-23 14:07:18 +0900

    feat: ChatService (createTask) edited

--- src/main/java/com/example/todo/service/chat/ChatService.java
+++ src/main/java/com/example/todo/service/chat/ChatService.java
@@ -15,16 +15,16 @@ import com.example.todo.domain.repository.user.UserRepository;
 import com.example.todo.dto.ChatMessageDto;
 import com.example.todo.dto.ChatRoomDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ChatService {
@@ -35,19 +35,24 @@ public class ChatService {
     private final MemberRepository memberRepository;
     private final TaskApiRepository taskApiRepository;
     public void createRoom(TaskApiEntity taskApiEntity) {
+        log.info(taskApiEntity.getUserId().toString());
+
         Optional<User> optionalUser = userRepository.findById(taskApiEntity.getUserId());
-        if (optionalUser.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        if (optionalUser.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "create chatroom 유저 조회X");
         User user = optionalUser.get();
+
+        log.info("2");
         Optional<TeamEntity> teamEntityOptional = teamReposiotry.findById(taskApiEntity.getTeam().getId());
         if (teamEntityOptional.isEmpty())
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 팀은 존재하지 않습니다!");
         TeamEntity teamEntity = teamEntityOptional.get();
 
+        log.info("3");
         Optional<MemberEntity> optionalMemberEntity = memberRepository.findByTeamAndUser(teamEntity, user);
         if (optionalMemberEntity.isEmpty())
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 유저는 해당 팀에 속한 멤버가 아닙니다!");
-//        MemberEntity memberEntity = optionalMemberEntity.get();
 
+        log.info("4");
         ChatRoom chatRoom = new ChatRoom();
         chatRoom.setTaskApiEntity(taskApiEntity);
         chatRoomRepository.save(chatRoom);
@@ -56,7 +61,6 @@ public class ChatService {
     public void saveChatMessage(ChatMessageDto chatMessageDto) {
         chatRepository.save(chatMessageDto.newEntity());
     }
-
     public String enterRoom(Long userId, Long teamId, Long taskId) {
         Optional<User> optionalUser = userRepository.findById(userId);
         if (optionalUser.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
