f0378bdb26eeeb499a1d26a91525c89289aa7223
acycoco <pink10257@naver.com>
2023-08-24 13:42:39 +0900

    feat: TodoApplication 실행 시 admin 계정 생성

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -1,13 +1,18 @@
 package com.example.todo;
 
+import com.example.todo.service.user.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class TodoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(TodoApplication.class, args);
+
+		UserService userService = context.getBean(UserService.class);
+		userService.createAdminUser();
 	}
 
 }
