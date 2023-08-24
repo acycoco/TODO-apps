3bb791757aa9ac06bf007e5d7ef6326ab6de2832
acycoco <pink10257@naver.com>
2023-08-24 09:39:32 +0900

    fix: Subscription 레포지토리 수정

--- src/main/java/com/example/todo/domain/repository/SubscriptionRepository.java
+++ src/main/java/com/example/todo/domain/repository/SubscriptionRepository.java
@@ -1,6 +1,7 @@
 package com.example.todo.domain.repository;
 
+import com.example.todo.domain.entity.SubscriptionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubscriptionRepository extends JpaRepository<SubscriptionRepository, Long> {
+public interface SubscriptionRepository extends JpaRepository<SubscriptionEntity, Long> {
 }
