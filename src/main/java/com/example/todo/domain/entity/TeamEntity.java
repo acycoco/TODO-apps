709a24b53dc126d9ce9f8bb83e335d79795f2b10
RiverCastle <rkdtjd12@gmail.com>
2023-08-31 13:30:45 +0900

    feat: TeamEntity, TaskCommentEntity, TodoCommentEntity edit (time stamp, team manager id)

--- src/main/java/com/example/todo/domain/entity/TeamEntity.java
+++ src/main/java/com/example/todo/domain/entity/TeamEntity.java
@@ -4,12 +4,13 @@ import com.example.todo.domain.entity.user.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
-public class TeamEntity {
+public class TeamEntity extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,4 +29,8 @@ public class TeamEntity {
 
     @OneToMany(mappedBy = "team")
     private List<TeamSubscriptionEntity> teamSubscriptions;
+
+    public Long getManagerId() {
+        return manager.getId();
+    }
 }
