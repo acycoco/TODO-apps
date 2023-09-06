8f8db9d84d81fd7055afb09fa857d56c00d3cdb8
acycoco <pink10257@naver.com>
2023-09-06 21:28:53 +0900

    feat: version컬럼 추가, 낙관적 락 로직 추가

--- src/main/java/com/example/todo/service/team/OptimisticLockTeamFacade.java
+++ src/main/java/com/example/todo/service/team/OptimisticLockTeamFacade.java
@@ -15,18 +15,19 @@ public class OptimisticLockTeamFacade {
     private final TeamService teamService;
 
     public void joinTeam(Long userId, TeamJoinDto teamJoinDto, Long teamId) throws InterruptedException {
-        for (int retryCount = 0; retryCount < 3; retryCount++) {
+//        for (int retryCount = 0; retryCount < 3; retryCount++) {
+        while (true) {
             try {
-                log.info("[RETRY_COUNT]: {}", retryCount);
+//                log.info("[RETRY_COUNT]: {}", retryCount);
                 teamService.joinTeam(userId, teamJoinDto, teamId);
                 break;
             } catch (ResponseStatusException e) {
                 log.info("retry stop: {}", e.getMessage());
                 break;
-            }
-            catch (OptimisticLockException | ObjectOptimisticLockingFailureException e) {
+            } catch (OptimisticLockException | ObjectOptimisticLockingFailureException e) {
                 Thread.sleep(50);
             }
         }
+//        }
     }
 }
