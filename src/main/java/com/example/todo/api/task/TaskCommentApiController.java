43478262fc3bf77cd6c7e6cebc84f2fe4d7946e9
RiverCastle <rkdtjd12@gmail.com>
2023-09-08 09:46:37 +0900

    feat: TaskCommentApiController, TodoCommentApiController edited

--- src/main/java/com/example/todo/api/task/TaskCommentApiController.java
+++ src/main/java/com/example/todo/api/task/TaskCommentApiController.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/team/{teamId}/task/{taskId}/comments")
 @RequiredArgsConstructor
 public class TaskCommentApiController {
