5e230aabe632e908e886d92f5ded2a16a9177d97
acycoco <pink10257@naver.com>
2023-09-11 00:57:36 +0900

    fix: IamportConfig 환경변수로 변경

--- src/main/java/com/example/todo/config/IamportConfig.java
+++ src/main/java/com/example/todo/config/IamportConfig.java
@@ -1,14 +1,19 @@
 package com.example.todo.config;
 
 import com.siot.IamportRestClient.IamportClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class IamportConfig {
+
+    @Value("${iamport.key}")
+    private String key;
+    @Value("${iamport.secret}")
+    private String secret;
     @Bean
     public IamportClient iamportClient() {
-        return new IamportClient(" ",
-                " ");
+        return new IamportClient(key, secret);
     }
 }
