e5e94ae662feb90d16eff7eb73367b1a167bdc6e
acycoco <pink10257@naver.com>
2023-09-11 00:50:19 +0900

    fix: dto 수정, 파일위치 변경

--- src/main/java/com/example/todo/dto/payment/PaymentInfoDto.java
+++ src/main/java/com/example/todo/dto/payment/PaymentInfoDto.java
@@ -1,13 +1,17 @@
-package com.example.todo.dto;
+package com.example.todo.dto.payment;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-@Data
+@Getter
+@Setter
 public class PaymentInfoDto {
     private String impUid; // 아임포트 고유 결제 번호
     private String merchantUid; // 가맹점 주문 번호
@@ -15,7 +19,6 @@ public class PaymentInfoDto {
     private String paidAt;
     private BigDecimal paidAmount;
     private String payMethod;
-    private String applyNum; // 카드 승인번호
     private String payStatus;
 
     public LocalDateTime convertFromUnixTimestamp(String paidAt) {
