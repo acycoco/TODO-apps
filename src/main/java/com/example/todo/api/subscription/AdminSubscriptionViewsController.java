d08a08b052959e86fc53ead40e98cb9e0895602e
acycoco <pink10257@naver.com>
2023-09-11 11:52:53 +0900

    feat: 구독권 프론트엔트 추가

--- src/main/java/com/example/todo/api/subscription/AdminSubscriptionViewsController.java
+++ src/main/java/com/example/todo/api/subscription/AdminSubscriptionViewsController.java
@@ -2,7 +2,9 @@ package com.example.todo.api.subscription;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,13 +17,26 @@ public class AdminSubscriptionViewsController {
         return "create-subscription";
     }
 
-    @GetMapping("/update-subscription")
-    public String updateSubscription(){
+    @GetMapping("/update-subscription/{subscriptionId}")
+    public String updateSubscription(
+            @PathVariable("subscriptionId") Long subscriptionId,
+            Model model
+    ){
+        model.addAttribute("subscriptionId", subscriptionId);
         return "update-subscription";
     }
 
     @GetMapping("/subscription")
     public String shoewSubscription(){
-        return "subscription";
+        return "all-subscription";
+    }
+
+    @GetMapping("/subscription/{subscriptionId}")
+    public String shoewSubscriptionDetails(
+            @PathVariable("subscriptionId") Long subscriptionId,
+            Model model
+    ){
+        model.addAttribute("subscriptionId", subscriptionId);
+        return "subscription-details";
     }
 }
