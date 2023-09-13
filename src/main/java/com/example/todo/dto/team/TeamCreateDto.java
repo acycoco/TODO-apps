ef37e78ac42c127cf067acd09efb0161a7a5f983
RiverCastle <rkdtjd12@gmail.com>
2023-09-13 10:28:42 +0900

    feat: TeamCreateDto (participantNumMax type changed) edited

--- src/main/java/com/example/todo/dto/team/TeamCreateDto.java
+++ src/main/java/com/example/todo/dto/team/TeamCreateDto.java
@@ -12,7 +12,7 @@ public class TeamCreateDto {
     private String description;
     //Validation 추가 필요
     private String joinCode;
-    private Integer participantNumMax;
+    private int participantNumMax;
 
     @Builder
     public TeamCreateDto(final String name, final String description, final String joinCode, final Integer participantNumMax) {
